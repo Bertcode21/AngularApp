81ca54f7500407fbfb2ddd0b68d7594cda2b307a
bertcode21 <ekongbert02@gmail.com>
2025-11-25 16:26:10 +0100

    Third commit

--- Bee-Shoping/src/app/header/header.component.ts
+++ Bee-Shoping/src/app/header/header.component.ts
@@ -1,10 +1,12 @@
 import { Component } from "@angular/core";
 import { TopHead } from "./top-menu/top-head";
+import { MainMenu } from "./main-menu/main-menu";
+
 
 @Component({
     selector: "app-header",
     templateUrl: "./header.component.html",
-     imports: [TopHead],
+     imports: [TopHead, MainMenu],
     styleUrls: ["./header.component.css"]
 })
 export class HeaderComponent {
